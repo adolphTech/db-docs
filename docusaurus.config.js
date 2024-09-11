665c70ec258f8c7375114760dc4e0317ba75c92f
adolphTech <adolphjohn0@gmail.com>
2024-09-11 13:27:33 +0300

    UPDATE:tables

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Made with ❤️. `,
     },
     prism: {
       theme: prismThemes.github,
