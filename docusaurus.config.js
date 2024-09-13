54e89908174a05eb5815a15584fe26cbd2bd46a7
adolphTech <adolphjohn0@gmail.com>
2024-09-13 14:28:13 +0300

    crawler :ADD

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,13 +84,128 @@ const config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Made with ❤️. `,
+      copyright: `Copyright © ${new Date().getFullYear()} Made with ❤️.`,
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
+    algolia: {
+      apiKey: '7f1b6f68058dc748f20bf856cc777fad',
+      indexName: '24',
+      appId: 'KWHP7AUTM2', // Optional, if you have it
+      contextualSearch: true, // Enables contextual search
+      searchParameters: {}, // Optional: Any search parameters you want to define
+    },
   }),
 };
 
 export default config;
+
+
+
+
+
+
+
+
+
+
+
+
+// // @ts-check
+// // `@type` JSDoc annotations allow editor autocompletion and type checking
+// // (when paired with `@ts-check`).
+// // There are various equivalent ways to declare your Docusaurus config.
+// // See: https://docusaurus.io/docs/api/docusaurus-config
+
+// import {themes as prismThemes} from 'prism-react-renderer';
+
+// /** @type {import('@docusaurus/types').Config} */
+// const config = {
+//   title: 'DB Documentation',
+//   tagline: 'Dinosaurs are cool',
+//   favicon: 'img/favicon.ico',
+
+//   // Set the production url of your site here
+//   url: 'https://your-docusaurus-site.example.com',
+//   // Set the /<baseUrl>/ pathname under which your site is served
+//   baseUrl: '/',
+
+//   onBrokenLinks: 'throw',
+//   onBrokenMarkdownLinks: 'warn',
+
+//   i18n: {
+//     defaultLocale: 'en',
+//     locales: ['en'],
+//   },
+
+//   presets: [
+//     [
+//       'classic',
+//       /** @type {import('@docusaurus/preset-classic').Options} */
+//       ({
+//         docs: {
+//           sidebarPath: './sidebars.js',
+//           editUrl: 'https://github.com/adolphTech/db-docs/blob/main',
+//         },
+//         theme: {
+//           customCss: './src/css/custom.css',
+//         },
+//       }),
+//     ],
+//   ],
+
+//   themeConfig: /** @type {import('@docusaurus/preset-classic').ThemeConfig} */ ({
+//     image: 'img/docusaurus-social-card.jpg',
+//     navbar: {
+//       title: 'Aloha !',
+//       logo: {
+//         alt: 'My Site Logo',
+//         src: 'img/logo.svg',
+//       },
+//       items: [
+//         {
+//           type: 'docSidebar',
+//           sidebarId: 'tutorialSidebar',
+//           position: 'left',
+//           label: 'Documentation',
+//         },
+//         {
+//           href: 'https://github.com/adolphTech',
+//           label: 'GitHub',
+//           position: 'right',
+//         },
+//       ],
+//     },
+//     footer: {
+//       style: 'dark',
+//       links: [
+//         {
+//           title: 'Community',
+//           items: [
+//             {
+//               label: 'Stack Overflow',
+//               href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+//             },
+//             {
+//               label: 'Discord',
+//               href: 'https://discordapp.com/invite/docusaurus',
+//             },
+//             {
+//               label: 'Twitter',
+//               href: 'https://twitter.com/docusaurus',
+//             },
+//           ],
+//         },
+//       ],
+//       copyright: `Copyright © ${new Date().getFullYear()} Made with ❤️. `,
+//     },
+//     prism: {
+//       theme: prismThemes.github,
+//       darkTheme: prismThemes.dracula,
+//     },
+//   }),
+// };
+
+// export default config;
